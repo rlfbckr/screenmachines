6192a80ebdd2057b24768ea08114c052855e4cad
rlfbckr <rlfbckr@gmail.com>
2021-03-31 11:34:02 +0200

    rm flatland

--- flatland/scripts/update_flatland_dns.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/home/rlfbckr/tools/godaddy-dyndns --api-key=e5CcvNGjVRb4_Q6vxpN7SZJj2JWHpVRRe2k --secret-key=AQPqnG34wmtKQQifwqt14o --domain=flatland.rlfbckr.io
-
-#&>> dyndsn.log
--- flatland/scripts/update_flatland_repository.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /home/rlfbckr/hfk/screenmachines
-pwd
-while true; do git pull; sleep 10; done
