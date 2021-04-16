c1b24fdf10007cfcfe8fe8fc3635ec48b49b9238
rlfbckr <rlfbckr@gmail.com>
2021-04-16 12:53:10 +0200

    update

--- examples/0_introduction/vera/sketch.js
+++ examples/0_introduction/vera/sketch.js
@@ -10,9 +10,9 @@ function setup() {
 }
 
 function draw() {
-  background(0);
-  stroke(255);
-  strokeWeight(1);
+  background(0); // hintergrundfarbe
+  stroke(255); // stiftfarbe
+  strokeWeight(1); // stiftdicke
   for (var y= 0; y<maxpoints;y++) {
     for (var x= 0; x<maxpoints;x++) {
       point(map(x,0,maxpoints-1,margin,width-margin),map(y,0,maxpoints-1,margin,width-margin));
