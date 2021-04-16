fa8a4587683aec8e5461bf4f906cf728af6b6465
rlfbckr <rlfbckr@gmail.com>
2021-04-16 12:30:04 +0200

    update

---
+++ examples/0_introduction/vera/sketch.js
@@ -0,0 +1,23 @@
+/*
+
+	vera molnars quadrat
+
+*/
+
+var maxpoints = 4; // wievile punkte
+var margin = 100; // rand
+
+function setup() {
+  createCanvas(600,600);
+}
+
+function draw() {
+  background(0);
+  stroke(255);
+  strokeWeight(1);
+  for (var y= 0; y<maxpoints;y++) {
+    for (var x= 0; x<maxpoints;x++) {
+      point(map(x,0,maxpoints-1,margin,width-margin),map(y,0,maxpoints-1,margin,width-margin));
+    }
+  }
+}
