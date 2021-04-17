c739dca17d7b3455326fafc3ff85058f04805c63
rlfbckr <rlfbckr@gmail.com>
2021-04-17 14:02:23 +0200

    update

--- examples/1_repetition-pattern-colors/rgb-color-field/sketch.js
+++ examples/1_repetition-pattern-colors/rgb-color-field/sketch.js
@@ -2,7 +2,7 @@
   rgb color field
 */
 
-var maxpoints = 5; // wieviele punkte
+var maxpoints = 6; // wieviele punkte
 var margin = 60; // wieviel rand
 var boxsize = 90;
 //var color_top_left, color_top_right, color_bottom_left, color_bottom_right;
@@ -13,8 +13,8 @@ function setup() {
   rectMode(CENTER);
   color_top_left = color(255, 0, 0); // rot
   color_top_right = color(0, 255, 0); // grün
-  color_bottom_left = color(0, 0, 255); // blau
-  color_bottom_right = color(255, 255, 0); // geld
+  color_bottom_left = color(255,255,0); // gelb
+  color_bottom_right = color(0, 0, 255); // blau
 }
 
 function draw() {
