4bdb5a2c6ccbd14444959612073f92e8a8862f37
rlfbckr <rlfbckr@gmail.com>
2021-04-18 11:59:49 +0200

    update

--- examples/3_moving-things/verticle_line_movement/sketch.js
+++ examples/3_moving-things/verticle_line_movement/sketch.js
@@ -1,39 +1,39 @@
 
 const maxpoints = 100;
-let points = [];
-
+var t = -20;
+var speed = 0.01;
+var trans = 120;
+var noisescale = 0.1;
+var noiseamp = 50;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  for (var i = 0; i < maxpoints; i++) {
-    let p = createVector(0, map(i, 0, maxpoints - 1, -100, height+100));
-    points.push(p);
-  }
   rectMode(CENTER);
   background(0);
 }
 
 function draw() {
- // background(0,100);
-  stroke(255,220);
+  background(0, 1);
+  if (t % 3 == 0) stroke(255, 0, 0, trans);
+  if (t % 3 == 1) stroke(0, 255, 0, trans);
+  if (t % 3 == 2) stroke(0, 0, 255, trans);
+
+
+  //stroke(255,100); // 100+noise(millis()*0.00001)*150,200);
   noFill();
   beginShape();
-  let allout = 0;
-  for (var i = 0; i < points.length; i++) {
-    let p = points[i];
-    vertex(p.x, p.y);
-    p.x = p.x + random(1, 3);
-    p.y = p.y + random(-2, 2);
-    if (p.x>=width) allout++;
+  for (var i = 0; i < maxpoints; i++) {
+    var x = t%width;
+    var y = map(i, 0, maxpoints - 1, -noiseamp, height+noiseamp);
+    x += noise(i*noisescale, t*speed) * noiseamp;
+y += map(noise(i*noisescale,t*speed),0,1,0,noiseamp);
+    vertex(x, y);
   }
-  if (allout == points.length) {
-    for (var i = 0; i < points.length; i++) {
-      let p = points[i];
-      p.x = 0;
-    }
-  }
+
   endShape();
+  t += 1;
+
 }
 
 function windowResized() {
-	resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight);
 }
