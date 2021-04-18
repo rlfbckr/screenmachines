5a70488f10f4f17bf528c6a94428cda11420ae35
rlfbckr <rlfbckr@gmail.com>
2021-04-18 12:01:34 +0200

    update

--- examples/3_moving-things/verticle_line_movement/sketch.js
+++ examples/3_moving-things/verticle_line_movement/sketch.js
@@ -9,6 +9,8 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
   background(0);
+  noFill();
+
 }
 
 function draw() {
@@ -16,22 +18,16 @@ function draw() {
   if (t % 3 == 0) stroke(255, 0, 0, trans);
   if (t % 3 == 1) stroke(0, 255, 0, trans);
   if (t % 3 == 2) stroke(0, 0, 255, trans);
-
-
-  //stroke(255,100); // 100+noise(millis()*0.00001)*150,200);
-  noFill();
   beginShape();
   for (var i = 0; i < maxpoints; i++) {
-    var x = t%width;
-    var y = map(i, 0, maxpoints - 1, -noiseamp, height+noiseamp);
-    x += noise(i*noisescale, t*speed) * noiseamp;
-y += map(noise(i*noisescale,t*speed),0,1,0,noiseamp);
+    var x = t % width;
+    var y = map(i, 0, maxpoints - 1, -noiseamp, height + noiseamp);
+    x += noise(i * noisescale, t * speed) * noiseamp;
+    y += map(noise(i * noisescale, t * speed), 0, 1, 0, noiseamp);
     vertex(x, y);
   }
-
-  endShape();
+ endShape();
   t += 1;
-
 }
 
 function windowResized() {
