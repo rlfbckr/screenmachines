9f3636c2096140e62be1b1f2371ff74842e3978a
rlfbckr <rlfbckr@gmail.com>
2021-04-23 12:13:36 +0200

    update

--- examples/1_repetition-pattern-colors/psychedelic-circles/sketch.js
+++ examples/1_repetition-pattern-colors/psychedelic-circles/sketch.js
@@ -1,38 +1,41 @@
 let osc, playing, freq, amp;
 
-var pointsPerCircle = 100;
+var pointsPerCircle = 40;
 var radius = 300;
 var drehung = 0;
 var t = 0;
-
+var blend = 0.2;
 function setup() {
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
   frameRate(25);
- osc = new p5.Oscillator('sine');
- osc.start();
 
- osc.freq(freq, 1000);
- osc.amp(amp, 0.5);
+
+  osc = new p5.Oscillator('sine');
+  osc.start();
+
+  osc.freq(freq, 1000);
+  osc.amp(amp, 0.5);
+  background(0,blend*255);
 }
 
 function draw() {
-//background(255); 
- 
-  if (t%3 == 0)  background(255,0,0);
-  if (t%3 == 1)  background(0,255,0);
-  if (t%3 == 2)  background(0,0,255);
- stroke(255);
+  background(0,6);
+  if (t % 3 == 0) stroke(255, 0, 0,200);
+  if (t % 3 == 1) stroke(0, 255, 0,200);
+  if (t % 3 == 2) stroke(0, 0, 255,200);
+strokeWeight(2);
+
   for (var winkel = 0; winkel < 360; winkel = winkel + (360 / pointsPerCircle)) {
     var xpos = cos(winkel + drehung) * radius;
     var ypos = sin(winkel + drehung) * radius;
     line(width / 2, height / 2, (width / 2) + xpos, (height / 2) + ypos);
   }
-  drehung = drehung+0.3; //map(mouseX,0,width-1,-10,10);
+  drehung = drehung + 0.5; //map(mouseX,0,width-1,-10,10);
   //radius = mouseY/2;
-   radius = 250 +(200 * sin(millis()*0.1));
-   osc.amp(map(radius,250,450,0.1,0.9),0.5); 
+  radius = 250 + (200 * sin(millis() * 0.05));
+  osc.amp(map(radius, 250, 450, 0.1, 0.9), 0.3);
 
-   osc.freq(drehung+ (10+(radius/2)),0.2); 
-   t++;
+  osc.freq(drehung + (10 + (radius / 2)), 0.2);
+  t++;
 }
