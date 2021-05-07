8f40886356ef01a457623bad3839891c5c018531
rlfbckr <rlfbckr@gmail.com>
2021-05-07 13:58:54 +0200

    update

--- examples/3_moving-things/echoeffect/sketch.js
+++ examples/3_moving-things/echoeffect/sketch.js
@@ -1,23 +1,35 @@
 
 function setup() {
-	createCanvas(windowWidth, windowHeight); // width height
+	createCanvas(700, 700); // width height
 	rectMode(CENTER);
 	frameRate(50);
 	// Rot, Grün, Blau , Transparnt
-	background(255, 0, 0,255);
+	background(255);
+	noFill();
+	strokeWeight(1);
+	noStroke();
+ 	//blendMode(LIGHTEST);
+	 // https://p5js.org/reference/#/p5/blendMode
+
 }
 
 function draw() {
-	//	background(0); // leere den bildschim und füll in 0;
-	background(255, 0, 0,50);
-	noFill();
-	strokeWeight(10);
-	stroke(255,255,255);
+	//clear();
+	background(255,10);
+	fill(0,255,0);
 	push();
-	translate(width / 2, height / 2);
+	translate(200, height / 2);
 	rotate(millis() * 0.0005);
 	rect(0, 0, 300, 300);
 	pop();
+
+	fill(0, 0, 255,23);
+	push();
+	translate(500, height / 2);
+	rotate(millis() * -0.005);
+	rect(0, 0, 300, 300);
+	pop();
+
 }
 
 
