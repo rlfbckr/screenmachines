fcefd8f7271a17745d74f3996232b06f96c0b6a6
rlfbckr <rlfbckr@gmail.com>
2021-05-21 12:13:26 +0200

    update

--- examples/5_interactive-responsive-bots/extended-bot/sketch.js
+++ examples/5_interactive-responsive-bots/extended-bot/sketch.js
@@ -1,61 +1,85 @@
 /*
   Ralf Baecker 2021
-  bouncing balls
+  extended bots ...
 */
 let myBots = [];
-
+let speed = 2;
+let gravitation;
 function setup() {
   createCanvas(windowWidth, windowHeight); // wie gross ist die zeichenfläche
+randomSeed(1);
   for (var i = 0; i < 10; i++) {
-    myBots.push(new Bot(random(0, width), random(0, height)));
+    bot = new Bot(random(0, width), random(0, height), random(15, 50));
+    myBots.push(bot);
   }
+  gravitation = createVector(0,10);
+  frameRate(60);
   background(0, 0, 255);
+  let a = 10;
+  let b = a;
+  print("b="+b);
+
+  let vectorA = createVector(1,2,3);
+  print("vectorA = " + vectorA.x+ " " + vectorA.y+ " " + vectorA.z);
+  let vectorB = vectorA; // .copy();
+  print("vectorB = " + vectorB.x+ " " + vectorB.y+ " " + vectorB.z);
+  vectorB.x = 100;
+  print("vectorB = " + vectorB.x+ " " + vectorB.y+ " " + vectorB.z);
+  print("vectorA = " + vectorA.x+ " " + vectorA.y+ " " + vectorA.z);
+
 }
 
 function draw() {
   background(0, 0, 255);
   for (var i = 0; i < myBots.length; i++) {
-    myBots[i].update();
-    if (myBots[i].alive == 0) {
+      myBots[i].update(); // verhalten
+     if (myBots[i].age()>myBots[i].lifetime) {
       myBots.splice(i, 1); // alten bot löschen
-      myBots.push(new Bot(random(0, width), random(0, height))); // neuen bot erzeugen!!
-    }
+       myBots.push(new Bot(random(0, width), random(0, height), random(15, 50))); // neuen bot erzeugen!!
+     }
+ 
+    
     myBots[i].draw();
 
   }
-fill(0,0,255);
-  text(frameRate(),40,40)
+  fill(0, 0, 255);
+  text(int(frameRate())+ " " + int(millis()), 40, 40)
 }
 
 class Bot {
-  constructor(x, y) {
+  constructor(x, y, size) {
     // initalisierung
     this.position = createVector(x, y);
     this.velocity = createVector(random(-5, 5), random(-5, 5));
-
+    this.birthtime = millis();
     this.color = color(255);
-    this.size = 50;
+    this.size = size;
     this.alive = 1;
+    this.lifetime = random(1000,2000);
+  }
+
+  age() {
+    return millis() - this.birthtime;
   }
 
   update() {
-    // verhalten
-    // bouncing ballls
-    this.position.add(this.velocity);
+  
+    this.modifiedvelocity = this.velocity.copy();
+    this.modifiedvelocity.mult(speed);
+    //this.position.add(gravitation);
+    this.position.add(this.modifiedvelocity);
     // einfallswinkel = ausfallswinkel
-    if (this.position.x <= 0) this.velocity.x*=-1;
-    if (this.position.x >= width) this.velocity.x*=-1;
-    if (this.position.y <= 0) this.velocity.y*=-1;
-    if (this.position.y >= height) this.velocity.y*=-1;
+    if (this.position.x <= 0) this.velocity.x *= -1;
+    if (this.position.x >= width) this.velocity.x *= -1;
+    if (this.position.y <= 0) this.velocity.y *= -1;
+    if (this.position.y >= height) this.velocity.y *= -1;
   }
 
   draw() {
     // aussehen / zeichen
-    noStroke(); //(255);
-    fill(this.color, 128);
-    ellipse(this.position.x, this.position.y, this.size, this.size)
-    stroke(255,128);
-    strokeWeight(1);
-   
+    // noStroke(); //(255);
+    noFill();
+    stroke(this.color, 128);
+    ellipse(this.position.x, this.position.y, this.size, this.size);
   }
 }
---
+++ examples/5_interactive-responsive-bots/reflecting-balls/sketch.js
@@ -0,0 +1,61 @@
+/*
+  Ralf Baecker 2021
+  bouncing balls
+*/
+let myBots = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight); // wie gross ist die zeichenfläche
+  for (var i = 0; i < 10; i++) {
+    myBots.push(new Bot(random(0, width), random(0, height)));
+  }
+  background(0, 0, 255);
+}
+
+function draw() {
+  background(0, 0, 255);
+  for (var i = 0; i < myBots.length; i++) {
+    myBots[i].update();
+    if (myBots[i].alive == 0) {
+      myBots.splice(i, 1); // alten bot löschen
+      myBots.push(new Bot(random(0, width), random(0, height))); // neuen bot erzeugen!!
+    }
+    myBots[i].draw();
+
+  }
+fill(0,0,255);
+  text(frameRate(),40,40)
+}
+
+class Bot {
+  constructor(x, y) {
+    // initalisierung
+    this.position = createVector(x, y);
+    this.velocity = createVector(random(-5, 5), random(-5, 5));
+
+    this.color = color(255);
+    this.size = 50;
+    this.alive = 1;
+  }
+
+  update() {
+    // verhalten
+    // bouncing ballls
+    //this.position.add(this.velocity);
+    // einfallswinkel = ausfallswinkel
+    if (this.position.x <= 0) this.velocity.x*=-1;
+    if (this.position.x >= width) this.velocity.x*=-1;
+    if (this.position.y <= 0) this.velocity.y*=-1;
+    if (this.position.y >= height) this.velocity.y*=-1;
+  }
+
+  draw() {
+    // aussehen / zeichen
+    noStroke(); //(255);
+    fill(this.color, 128);
+    ellipse(this.position.x, this.position.y, this.size, this.size)
+    stroke(255,128);
+    strokeWeight(1);
+   
+  }
+}
