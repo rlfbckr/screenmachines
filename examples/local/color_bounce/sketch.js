dbb69965b65bedcb316542c57b0ed3d85636d209
rlfbckr <rlfbckr@gmail.com>
2022-04-27 11:13:30 +0200

    update

---
+++ examples/local/color_bounce/sketch.js
@@ -0,0 +1,17 @@
+var hintergrund_farbe = 0;
+var fade_direction = +1;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+}
+
+function draw() { // wird dauernd im loop aufgerufen
+	background(hintergrund_farbe);
+	hintergrund_farbe=hintergrund_farbe+fade_direction;
+	if (hintergrund_farbe >= 255) {
+		fade_direction= -1;
+	}
+	if (hintergrund_farbe <=0) {
+		fade_direction= 1;
+	}
+}
---
+++ examples/local/repetion_demo/sketch.js
@@ -0,0 +1,21 @@
+var rand = 150;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(255);
+	noLoop();
+
+}
+
+function draw() { // wird dauernd im loop aufgerufen
+	strokeWeight(1);
+	for (var y=rand;y<=windowHeight-rand;y=y+1) {
+		//print(i);
+		stroke(
+			map(y,rand,windowHeight-rand,0,255),
+			map(y,rand,windowHeight-rand,255,0),
+			map(y,rand,windowHeight-rand,0,127)
+		);
+		line(rand,y,windowWidth-rand,y);	
+	}
+}
